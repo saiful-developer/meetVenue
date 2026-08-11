48b49822cc58d9555184a2948c0f2c28c3037437
Saiful Islam <saifulislam@bdcom.net>
2026-08-11 17:57:17 +0600

    setup serverside

--- meet-venue-server/src/main.ts
+++ meet-venue-server/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // main.ts
+app.enableCors({
+  origin: ['*'],
+  credentials: true,
+});
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
